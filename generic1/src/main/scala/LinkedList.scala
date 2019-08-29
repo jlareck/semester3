c17216569a13053c86d00b06f2ebd603cf69b2af
jlareck <medik2507@gmail.com>
2019-08-29 09:33:25 +0300

    implemented merge sort

--- generic1/src/main/scala/LinkedList.scala
+++ generic1/src/main/scala/LinkedList.scala
@@ -1,16 +1,18 @@
 
-
+//package scala
 import scala.annotation.tailrec
-
-
+import scala.annotation.unchecked.uncheckedVariance
+import scala.collection.immutable.List
 import scala.math.Ordering
+import scala.runtime.Statics.releaseFence
 
 
 
 sealed abstract class LinkedList[+E] {
 
   def size : Int
-
+  def head: E
+  def tail: LinkedList[E]
   def foreach(f : (E) => Unit) {
     @tailrec def loop( items : LinkedList[E] ) {
       items match {
@@ -43,6 +45,15 @@ sealed abstract class LinkedList[+E] {
     (item, acc) =>
       Node(f(item), acc)
   }
+
+
+
+  def take(n: Int): LinkedList[E] =
+    if (n == 0 || size==0) Empty else head :: tail.take(n-1)
+  def drop(n: Int): LinkedList[E] =
+    if (n == 0 || size == 0) this else tail.drop(n-1)
+
+
 
   def reverse(): LinkedList[E] = {
     foldLeft(LinkedList[E]()) {
@@ -96,15 +107,35 @@ case class Node[E](head: E, tail: LinkedList[E]) extends LinkedList[E]{
 
 case object Empty extends LinkedList[Nothing]{
   val size = 0
+  override def head: Nothing = throw new NoSuchElementException("head of empty list")
+  override def tail: LinkedList[Nothing] = throw new UnsupportedOperationException("tail of empty list")
 }
 
-final case class ::[E<:Ordering[E]](private var hd: E, private var tl: LinkedList[E]) extends LinkedList[E] {
-  def head : E = hd
-  def tail : LinkedList[E] = tl
-  def isEmpty: Boolean = false
-  val size = 1
+final case class :: [E]( private var hd: E, private var tl: LinkedList[E]) extends LinkedList[E] {
+  releaseFence()
 
+  override def size = 1
+
+  override def head : E = hd
+  override def tail : LinkedList[E] = tl
 }
+
+
+
+//object :: {
+//  def unapply[E](ls: LinkedList[E]): Option[(E, E)] = {
+//    if (ls.size == 0) None
+//    else Some(ls.head1, ls.tail1)
+//  }
+//}
+//final case class ::[E<:Ordering[E]](private var hd: E, private var tl: LinkedList[E]) extends LinkedList[E] {
+//  def head : E = hd
+//  def tail : LinkedList[E] = tl
+//  def isEmpty: Boolean = false
+//  val size = 1
+//  releaseFence()
+//
+//}
 object LinkedList extends App{
 
   def apply[E](items: E*): LinkedList[E] = {
@@ -137,35 +168,47 @@ object LinkedList extends App{
 //    }
 //    _split(n, List(), l)
 //  }
-
-  def split[E](n: Int, l: LinkedList[E]):(LinkedList[E], LinkedList[E]) = {
-    def _split[E](c: Int, res: LinkedList[E], rem: LinkedList[E]):(LinkedList[E],LinkedList[E]) = (c, rem) match {
-      case (_, Empty) => (res, Empty)
-      case (0, rem) => (res, rem)
-      case (c, h :: tail) => _split(c - 1, res:::LinkedList(h), tail)
-    }
-    _split(n, LinkedList(), l)
-  }
-  def merge[E](left: LinkedList[E], right: LinkedList[E]) (implicit org: Ordering[E]): LinkedList[E] =
-    (left, right) match {
-      case(left, Empty) => left
-      case(Empty, right) => right
-      case(leftHead :: leftTail, rightHead :: rightTail) =>
-        if (org.compare(leftHead, rightHead)<0) leftHead::merge(leftTail, right)
-        else rightHead :: merge(left, rightTail)
-    }
-//
-  def mergeSort[E](list: LinkedList[E])(implicit org: Ordering[E]): LinkedList[E] = {
-    val n = list.size / 2
-    if (n == 0) list // i.e. if list is empty or single value, no sorting needed
-    else {
-      val (left, right) = split(n, list)
-      merge(mergeSort(left), mergeSort(right))
-    }
-  }
+def msort[E](xs: LinkedList[E])(implicit org: Ordering[E]): LinkedList[E] = {
+  def merge(xs1: LinkedList[E], xs2: LinkedList[E]): LinkedList[E] =
+  if (xs1.size == 0) xs2
+  else if (xs2.size == 0) xs1
+  else if (org.compare(xs1.head, xs2.head)<0) xs1.head :: merge(xs1.tail, xs2)
+  else xs2.head :: merge(xs1, xs2.tail)
+  val n = xs.size/2
+  if (n == 0) xs
+  else merge(msort(xs take n), msort(xs drop n))
+}
+//  def split[E](n: Int, l: LinkedList[E]):(LinkedList[E], LinkedList[E]) = {
+//    def _split[E](c: Int, res: LinkedList[E], rem: LinkedList[E]):(LinkedList[E],LinkedList[E]) = (c, rem) match {
+//      case (_, Empty) => (res, Empty)
+//      case (0, rem) => (res, rem)
+//      case (c, h :: tail) => _split(c - 1, res:::LinkedList(h), tail)
+//    }
+//    _split(n, LinkedList(), l)
+//  }
+//  def merge[E](left: LinkedList[E], right: LinkedList[E]) (implicit org: Ordering[E]): LinkedList[E] =
+//    (left, right) match {
+//      case(left, Empty) => left
+//      case(Empty, right) => right
+//      case(leftHead :: leftTail, rightHead :: rightTail) =>
+//        if (org.compare(leftHead, rightHead)<0) leftHead::merge(leftTail, right)
+//        else rightHead :: merge(left, rightTail)
+//    }
+////
+//  def mergeSort[E](list: LinkedList[E])(implicit org: Ordering[E]): LinkedList[E] = {
+//    val n = list.size / 2
+//    if (n == 0) list // i.e. if list is empty or single value, no sorting needed
+//    else {
+//      val (left, right) = split(n, list)
+//      merge(mergeSort(left), mergeSort(right))
+//    }
+//  }
   var a = LinkedList(3,25,5)
   a = a.::(2)
+ //print(a.head1)
+  //a.someTest
   var b = List(2,4,5)
-  mergeSort(a)
+  //print(a)
+ print(msort(a))
 }
 
