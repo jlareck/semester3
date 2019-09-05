2114faacad4445d797925df6c333941a998c0c7d
jlareck <medik2507@gmail.com>
2019-09-05 21:35:30 +0300

    some changes

--- generic1/src/main/scala/HelloWorld.scala
+++ generic1/src/main/scala/HelloWorld.scala
@@ -1,7 +1,26 @@
-
+import LinkedList.quickSort
 
 object HelloWorld extends App {
-    def square(x: Int) = x * x
 
+    //  var a = new FileClass("aa", 1, "ww")
+    //  va b = LinkedList(a)
+    //   val c = new FileClass("bb", 2, "qq")
+    //  b = b.::c
+    var a = LinkedList(3,25,5,8,0,-1,8)
+    a = a.::(2)
+    // //print(a.head1)
+    //  //a.someTest
+    //  var b = List(2,4,5)
+    //  //print(a)
+    // print(mergeSort(a))
 
+    //  val a = new FileClass("aa", 2, "ww")
+    //  val c = new FileClass("bb", 3, "qq")
+    //  var b = LinkedList(a,c)
+    //
+    //  b = b.::(new FileClass("bb", 2, "qq"))
+    //  print(insertionSort(b)(FileClass))
+    print(quickSort(a))
+
+    //println("a"<"b")
 }
