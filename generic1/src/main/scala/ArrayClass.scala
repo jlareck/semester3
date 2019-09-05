9f0ad0d3367ad9a3d332d5402526da0e459fd978
jlareck <medik2507@gmail.com>
2019-09-05 10:16:09 +0300

    implemented sort functions

--- generic1/src/main/scala/ArrayClass.scala
+++ generic1/src/main/scala/ArrayClass.scala
@@ -1,14 +1,71 @@
+
+
+import java.nio.file.Files
+
+import scala.reflect._
+import scala.math.Ordering
 object ArrayClass extends App{
-  def sort[E](xs: Array[E])(implicit org:Ordering[E]): Array[E] = {
+  def quickSort[E: ClassTag](xs: Array[E])(implicit org: Ordering[E]): Array[E] = {
     if (xs.length <= 1) xs
     else {
       val pivot = xs(xs.length / 2)
       Array.concat(
-        sort(xs filter (org.compare(_, pivot)<0)),
-        xs filter (pivot ==),
-        sort(xs filter( org.compare(_, pivot)>0)))
+        quickSort(xs.filter(org.compare(pivot,_)>0)),
+        xs.filter(org.compare(pivot,_)==0),
+        quickSort(xs.filter(org.compare(pivot,_)<0)))
     }
+  }
+  def mergeSort[E](xs: List[E])(implicit org: Ordering[E]): List[E] = {
+    def merge(xs1: List[E], xs2: List[E]): List[E] = (xs1, xs2) match {
+      case(_, Nil) => xs1
+      case(Nil,_) => xs2
+      case (x1 :: xtl1, x2 :: xtl2) =>  if (org.compare(x1, x2)<0)  x1  :: merge(xtl1, xs2) else x2 :: merge(xs1, xtl2)
+
     }
-    val a = Array(1,2,4,5)
-    print(sort(a))
+    val n = xs.length/2
+    if (n == 0) xs
+    else merge(mergeSort(xs take n), mergeSort(xs drop n))
+  }
+
+  def insertElement[T](elm: T, sorted: List[T])(implicit org: Ordering[T]): List[T] =
+    sorted match {
+      case Nil => elm :: sorted
+      case head :: tail if org.compare(head, elm) <0 => head :: insertElement(elm, tail)
+      case _ => elm :: sorted
+    }
+
+  def insertionSort[T](list: List[T])(implicit org: Ordering[T]): List[T] =
+    list match {
+      case Nil => list
+      case head :: tail =>
+        val sorted = insertionSort(tail)
+        insertElement(head, sorted)
+    }
+
+
+
+  //implicit val order = Ordering.by[FileClass, Int]
+    val a = new FileClass("aa", 2, "ww")
+    val c = new FileClass("bb", 3, "qq")
+  val d = new FileClass("bb", 1, "qq")
+  val q = new FileClass("bb", 2, "qe")
+    var b = Array(a,c,d, q)
+   // implicit val org = Ordering[FileClass]
+  val clsTag = classTag[FileClass]
+ // b = quickSort(b)(clsTag, FileClass.ordering)
+    //b.foreach(println)
+  //  println("b">"a")
+
+
+    var l = b.toList
+  l.foreach(print)
+   // l = mergeSort(l)
+  l = insertionSort(l)
+
+  val r = l.toArray
+
+  r.foreach(println)
+  //var a = Array(1,2,2,51,2,3,64,4)
+//  a = sort(a)(clsTag, FileClass.ordering)
+
 }
