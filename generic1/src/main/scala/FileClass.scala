1ddd66a7b0b3f5dcd821414c52854b6b6421f543
jlareck <medik2507@gmail.com>
2019-09-17 11:06:43 +0300

    deleted singleton obj

--- generic1/src/main/scala/FileClass.scala
+++ generic1/src/main/scala/FileClass.scala
@@ -1,19 +1,6 @@
+import java.text.SimpleDateFormat
 import java.util.Date
-import  scala.math.Ordering
-case class FileClass(var name:String, var size: Int, var localType: String)
-object FileClass extends Ordering[FileClass]{
-    def compare(x: FileClass, y: FileClass): Int = {
-    if (x.size != y.size) if (x.size < y.size) return 1
-    else return -1
 
-    if (x.name != y.name) if (x.name.compareTo(y.name) < 0) return -1
-    else return 1
+import scala.math.Ordering
+class FileClass(var name:String, var size: Int, var localType: String, var date: SimpleDateFormat ) extends Item
 
-
-    x.localType.compareTo(y.localType)
-
-
-  }
-  implicit def ordering[A <: FileClass]: Ordering[A] = Ordering.by(e => (e.size, e.name, e.localType))
-
-}
