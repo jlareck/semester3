9070d4eb2c9990574c879669d0442d9a1bdd1c65
jlareck <medik2507@gmail.com>
2019-09-22 14:21:43 +0300

    wrote some tests

--- generic1/src/test/scala/FileSystemTest.scala
+++ generic1/src/test/scala/FileSystemTest.scala
@@ -11,29 +11,37 @@ class FileSystemTest extends FunSuite {
     for FileClass and Folder - subtypes of class Item, for data structures linked list, array and list - ad-hoc (overloading)
     and coercion polymorphism (implicit conversions). So if you want to sort array with merge sort or insertion sort
     it will convert array to list and sort it as list. Also if you want to sort list with quicksort
-    it will implicitly convert it to array. But there is a problem that when I use for example function,
-    that uses implicit conversion, I need to return back it to first state(condition), so there is solution,
-    to handle it with boolean variable, but in unit tests I only checked if functions work. And I also used
+    it will implicitly convert it to array. And I also used
     overloaded sorting functions because there is linked list - the structure that is not library and
     I can't use toList or toArray for it. You will also ask where is logic in parameter of type array in quicksort
     and list in others, the answer is - I don't know ¯\_(ツ)_/¯.
   */
 
-  test("ArrayClass.sort") {
+  test("Array and list sorting") {
     //var flag: Boolean = false
 
     implicit def arrayToList[A](a: Array[A]) = a.toList
 
     implicit def listToArray[A: ClassTag](a: List[A]) = a.toArray
 
-    val list1 = List(1, 221, 331, 35, 6, 21, 5)
-    assert(SortedAlgorithms.mergeSort(list1) === List(1, 5, 6, 21, 35, 221, 331))
-    assert(SortedAlgorithms.insertionSort(list1) === List(1, 5, 6, 21, 35, 221, 331))
-    assert(SortedAlgorithms.quickSort(list1.toArray) === Array(1, 5, 6, 21, 35, 221, 331))
-    val list2 = Array("a", "s", "b", "r", "c","k") // можна було var
+    val array = Array(1, 221, 331, 35, 6, 21, 5)
+    assert(SortedAlgorithms.mergeSort(array) === List(1, 5, 6, 21, 35, 221, 331))
+    assert(SortedAlgorithms.insertionSort(array) === List(1, 5, 6, 21, 35, 221, 331))
+    assert(SortedAlgorithms.quickSort(array) === Array(1, 5, 6, 21, 35, 221, 331))
 
-    assert(SortedAlgorithms.quickSort(list2)===Array("a", "b","c", "k", "r", "s" ))
-//    if (flag){
+    val list = List("a", "s", "b", "r", "c","k")
+    assert(SortedAlgorithms.quickSort(list)===List("a", "b","c", "k", "r", "s" ))
+    assert(SortedAlgorithms.mergeSort(list)===List("a", "b","c", "k", "r", "s" ))
+    assert(SortedAlgorithms.insertionSort(list)===List("a", "b","c", "k", "r", "s" ))
+
+
+    val objList1 = FileManager.recursiveInitializing(FileManager.input_files)
+    val objList2 = objList1
+    assert(SortedAlgorithms.mergeSort(objList1) === SortedAlgorithms.insertionSort(objList2))
+    assert(SortedAlgorithms.quickSort(objList1) === SortedAlgorithms.insertionSort(objList2))
+
+
+    //    if (flag){
 //      list2 = list2.toArray
 //    }
 //    assert(ArrayClass.mergeSort(list2)===List("a", "b","c", "k", "r", "s" ))
