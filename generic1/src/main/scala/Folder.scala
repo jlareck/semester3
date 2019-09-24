3ecaa9d15dc609ecf421673a644c590337f06322
jlareck <medik2507@gmail.com>
2019-09-24 13:38:45 +0300

    added some two lists for folders and files

--- generic1/src/main/scala/Folder.scala
+++ generic1/src/main/scala/Folder.scala
@@ -1,19 +1,2 @@
-import scala.math.Ordering
+class Folder(var name: String,var localType: String, var modificationTime: String, var path: String, var folders: List[Folder], var files: List[MyFile]) extends Item
 
-class Folder(var name:String, var size: Int, var localType: String) extends Item
-//object Folder extends Ordering[Folder]{
-//  def compare(x: Folder, y: Folder): Int = {
-//    if (x.size != y.size) if (x.size < y.size) return 1
-//    else return -1
-//
-//    if (x.name != y.name) if (x.name.compareTo(y.name) < 0) return -1
-//    else return 1
-//
-//
-//    x.localType.compareTo(y.localType)
-//
-//
-//  }
-//  implicit def ordering[A <: Folder]: Ordering[A] = Ordering.by(e => (e.size, e.name, e.localType))
-//
-//}
