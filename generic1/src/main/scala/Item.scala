0384f2028f6860f0aa6f8b10eab7da14caab11b6
jlareck <medik2507@gmail.com>
2019-09-24 17:55:03 +0300

    some changes

--- generic1/src/main/scala/Item.scala
+++ generic1/src/main/scala/Item.scala
@@ -2,21 +2,19 @@ import scala.math.Ordering
 
 trait Item{
     var name:String
-    var size: Int
+    var modificationTime: String
     var localType: String
+    var path: String
 }
 object Item extends Ordering[Item]{
     def compare(x: Item, y: Item): Int = {
-        if (x.size != y.size) if (x.size < y.size) return 1
-        else return -1
-
         if (x.name != y.name) if (x.name.compareTo(y.name) < 0) return -1
         else return 1
 
+        if (x.modificationTime != y.modificationTime) if (x.modificationTime < y.modificationTime) return 1
+        else return -1
 
         x.localType.compareTo(y.localType)
-
-
     }
-    implicit def ordering[A <: Item]: Ordering[A] = Ordering.by(e => (e.size, e.name, e.localType))
+    implicit def ordering[A <: Item]: Ordering[A] = Ordering.by(e => (e.name, e.modificationTime, e.localType))
 }
