63c4aeafc91013fba7d29f04c7f6093b3da96123
jlareck <medik2507@gmail.com>
2019-09-24 22:50:24 +0300

    First version lab1

--- generic1/src/main/scala/FileClass.scala
+++
@@ -1,6 +0,0 @@
-import java.text.SimpleDateFormat
-import java.util.Date
-
-import scala.math.Ordering
-class FileClass(var name:String, var size: Int, var localType: String, var date: SimpleDateFormat ) extends Item
-
